a3a1dcea7b7d21b93ff184a7cd42b8df04b87e90
藤田守太 <fujishu0407@gmail.com>
2020-12-23 16:17:47 +0900

    リプライ機能チェック

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text="うどん大好き"))
 
 
 if __name__ == "__main__":
