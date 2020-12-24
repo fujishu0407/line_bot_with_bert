26831042de9b0bb2112fb7fcafe34d32c1d03077
藤田守太 <fujishu0407@gmail.com>
2020-12-24 13:23:24 +0900

    init導入

--- main.py
+++ main.py
@@ -11,12 +11,7 @@ from bert_ebdm_system import BertEbdmSystem
 
 app = Flask(__name__)
 
-#環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = "NcSKlH2/i1Gvv2eW38+VMRSSPuKgxY+vMtqUWcU5GzmpwWJYv6QiIuI+LtWLV2eIaSLiAv/egHlpnVM0ha3cDDUTHZeIqhzhqZiz8ve8FDlFYGB956+E9ZKUwdvXYc9oRJnKVfAF4cfxPHiLTl/+jwdB04t89/1O/w1cDnyilFU="
-YOUR_CHANNEL_SECRET = "3b6a26385dfd37f84997ed2394f6def8"
 
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -35,6 +30,17 @@ def callback():
 
     return 'OK'
 
+@app.route("/init", methods=['POST'])
+def init_system():
+    subprocess.call(["elasticsearch/bin/elasticsearch", "-d", "-p", "pid"])
+    system = BertEbdmSystem()
+    #環境変数取得
+    YOUR_CHANNEL_ACCESS_TOKEN = "NcSKlH2/i1Gvv2eW38+VMRSSPuKgxY+vMtqUWcU5GzmpwWJYv6QiIuI+LtWLV2eIaSLiAv/egHlpnVM0ha3cDDUTHZeIqhzhqZiz8ve8FDlFYGB956+E9ZKUwdvXYc9oRJnKVfAF4cfxPHiLTl/+jwdB04t89/1O/w1cDnyilFU="
+    YOUR_CHANNEL_SECRET = "3b6a26385dfd37f84997ed2394f6def8"
+
+    line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+    handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -45,9 +51,7 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-#    app.run()
-    subprocess.call(["elasticsearch/bin/elasticsearch", "-d", "-p", "pid"])
     port = int(os.environ.get("PORT", 5000))
     print(port)
     app.run(host="0.0.0.0", port=port)
-    system = BertEbdmSystem()
+    
