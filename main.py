6a876b9facf80fd619df7f13fff88f5a70b3f9cf
藤田守太 <fujishu0407@gmail.com>
2020-12-24 13:18:15 +0900

    一時的に動いてもらう

--- main.py
+++ main.py
@@ -49,5 +49,5 @@ if __name__ == "__main__":
     subprocess.call(["elasticsearch/bin/elasticsearch", "-d", "-p", "pid"])
     port = int(os.environ.get("PORT", 5000))
     print(port)
-    system = BertEbdmSystem()
     app.run(host="0.0.0.0", port=port)
+    system = BertEbdmSystem()
